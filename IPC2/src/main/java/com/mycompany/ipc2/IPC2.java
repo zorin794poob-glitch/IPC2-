117af5600b80a479ec831ba5816815900fb36a0e
zorin794 <zorin794poob@gmail.com>
2026-02-03 21:10:23 -0600

    listoipc

---
+++ IPC2/src/main/java/com/mycompany/ipc2/IPC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc2;
+
+/**
+ *
+ * @author jonathan
+ */
+public class IPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
